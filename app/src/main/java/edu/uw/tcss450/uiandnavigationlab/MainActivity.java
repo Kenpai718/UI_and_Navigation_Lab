c41d1ff35f7b3852ea9ab9d339d8edac84d73907
Ken Ahrens <75004340+Kenpai718@users.noreply.github.com>
2022-04-12 18:38:22 -0700

    Set up initial Bottom Nav (step 75)

--- app/src/main/java/edu/uw/tcss450/uiandnavigationlab/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/uiandnavigationlab/MainActivity.java
@@ -2,14 +2,21 @@ package edu.uw.tcss450.uiandnavigationlab;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.auth0.android.jwt.JWT;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import android.os.Bundle;
 
 import edu.uw.tcss450.uiandnavigationlab.model.UserInfoViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +50,18 @@ public class MainActivity extends AppCompatActivity {
 
         //keep at end of oncreate
         setContentView(R.layout.activity_main);
+
+        // Make sure the new statements go BELOW setContentView
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_incrementor, R.id.navigation_blogs)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 
 }
